621b5281507b7259cafca4f725d909a3237ca578
Juanjo Asensio García <asengar2009@gmail.com>
2023-09-09 04:27:48 +0200

    Make the text editor larger

--- src/components/textEditor.jsx
+++ src/components/textEditor.jsx
@@ -7,65 +7,66 @@ import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext
 import LexicalErrorBoundary from '@lexical/react/LexicalErrorBoundary';
 
 const theme = {
-  // Theme styling goes here
+    // Theme styling goes here
 }
 
 export function TextEditor({ t }) {
-  const initialConfig = {
-    namespace: "MathEditor",
-    theme,
-    onError,
-  };
+    const initialConfig = {
+        namespace: "MathEditor",
+        theme,
+        onError,
+    };
 
-  const [editorState, setEditorState] = useState();
-  function onChange(editorState) {
-    console.log(`Current editor state: ${JSON.stringify(editorState)}`)
-    setEditorState(editorState);
-  }
+    const [editorState, setEditorState] = useState();
+    function onChange(editorState) {
+        console.log(`Current editor state: ${JSON.stringify(editorState)}`)
+        setEditorState(editorState);
+    }
 
-  return (
-    <LexicalComposer initialConfig={initialConfig}>
-      <RichTextPlugin
-        contentEditable={<ContentEditable />}
-        placeholder={<div>{t("editor.placeholder")}</div>}
-        ErrorBoundary={LexicalErrorBoundary}
-      />
-      <HistoryPlugin />
-      <AutoFocus />
-      <OnChangePlugin onChange={onChange} />
-    </LexicalComposer>
-  );
+    return (
+        <LexicalComposer initialConfig={initialConfig}>
+            <RichTextPlugin
+                contentEditable={<ContentEditable className="h-100" />}
+                placeholder={
+                    <div>{t("editor.placeholder")}</div>
+                }
+                ErrorBoundary={LexicalErrorBoundary}
+            />
+            <HistoryPlugin />
+            <AutoFocus />
+            <OnChangePlugin onChange={onChange} />
+        </LexicalComposer>
+    );
 }
 
 // Catch any errors that occur during Lexical updates and log them
 // or throw them as needed. If you don't throw them, Lexical will
 // try to recover gracefully without losing user data.
 function onError(error) {
-  console.error(error);
+    console.error(error);
 }
 
 function AutoFocus() {
-  const [editor] = useLexicalComposerContext();
+    const [editor] = useLexicalComposerContext();
 
-  useEffect(() => {
-    // Focus the editor when the effect fires!
-    editor.focus();
-  }, [editor]);
+    useEffect(() => {
+        // Focus the editor when the effect fires!
+        editor.focus();
+    }, [editor]);
 
-  return null;
+    return null;
 }
 
 // When the editor changes
 function OnChangePlugin({ onChange }) {
-  // Access the editor through the LexicalComposerContext
-  const [editor] = useLexicalComposerContext();
-  // Wrap our listener in useEffect to handle the teardown and avoid stale references.
-  useEffect(() => {
-    // most listeners return a teardown function that can be called to clean them up.
-    return editor.registerUpdateListener(({ editorState }) => {
-      // call onChange here to pass the latest state up to the parent.
-      onChange(editorState);
-    });
-  }, [editor, onChange]);
-
+    // Access the editor through the LexicalComposerContext
+    const [editor] = useLexicalComposerContext();
+    // Wrap our listener in useEffect to handle the teardown and avoid stale references.
+    useEffect(() => {
+        // most listeners return a teardown function that can be called to clean them up.
+        return editor.registerUpdateListener(({ editorState }) => {
+            // call onChange here to pass the latest state up to the parent.
+            onChange(editorState);
+        });
+    }, [editor, onChange]);
 }
