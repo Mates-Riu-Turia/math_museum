3c85ada2278eddf421b039e86a5d6e688c18f3db
Juanjo Asensio García <asengar2009@gmail.com>
2023-09-19 17:17:40 +0200

    Add format text buttons and its functionality

--- src/components/textEditorToolbar.jsx
+++ src/components/textEditorToolbar.jsx
@@ -1,13 +1,17 @@
 import { useState, useEffect } from "react";
-import { Stack, Button, OverlayTrigger, Tooltip } from "react-bootstrap";
+import { Stack, Button, OverlayTrigger, Tooltip, ToggleButton } from "react-bootstrap";
 import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
-import { UNDO_COMMAND, CAN_UNDO_COMMAND, REDO_COMMAND, CAN_REDO_COMMAND } from "lexical";
+import { UNDO_COMMAND, CAN_UNDO_COMMAND, REDO_COMMAND, CAN_REDO_COMMAND, SELECTION_CHANGE_COMMAND, COMMAND_PRIORITY_CRITICAL, FORMAT_TEXT_COMMAND } from "lexical";
+import { $getSelection, $isRangeSelection } from "lexical"
 
 export function Toolbar({ t }) {
     return (
         <Stack direction="horizontal" className="border rounded-2 w-100 border-primary mb-2">
             <ToolbarContainer>
                 <History />
+            </ToolbarContainer>
+            <ToolbarContainer>
+                <Format />
             </ToolbarContainer>
         </Stack>
     );
@@ -23,11 +27,11 @@ function History() {
         editor.registerCommand(CAN_UNDO_COMMAND, (payload) => {
             setCanUndo(payload);
             return true;
-        }, 4);
+        }, COMMAND_PRIORITY_CRITICAL);
         editor.registerCommand(CAN_REDO_COMMAND, (payload) => {
             setCanRedo(payload);
             return true;
-        }, 4);
+        }, COMMAND_PRIORITY_CRITICAL);
     }, []);
 
     const undo = () => {
@@ -35,7 +39,7 @@ function History() {
     };
 
     const redo = () => {
-        editor.dispatchCommand(REDO_COMMAND)
+        editor.dispatchCommand(REDO_COMMAND);
     }
 
     return (
@@ -50,10 +54,51 @@ function History() {
     );
 }
 
+function Format() {
+    const [editor] = useLexicalComposerContext();
+
+    const [isBold, setIsBold] = useState(false);
+    const [isItalic, setIsItalic] = useState(false);
+    const [isUnderlined, setIsUnderlined] = useState(false);
+
+    useEffect(() => {
+        editor.registerCommand(SELECTION_CHANGE_COMMAND, () => {
+            editor.update(() => {
+                const selection = $getSelection();
+
+                if ($isRangeSelection(selection)) {
+                    setIsBold(selection.hasFormat("bold"));
+                    setIsItalic(selection.hasFormat("italic"));
+                    setIsUnderlined(selection.hasFormat("underline"));
+                }
+            })
+            return true;
+        }, COMMAND_PRIORITY_CRITICAL)
+    }, [setIsBold]);
+
+    const setFormat = (format) => {
+        editor.dispatchCommand(FORMAT_TEXT_COMMAND, format);
+    };
+
+    return (
+        <>
+            <ToolbarButton title={["Bold (", <kbd>Ctrl</kbd>, "+", <kbd>B</kbd>, ")"]} checked={isBold} onClick={() => setFormat("bold")}>
+                <i className="bi bi-type-bold"></i>
+            </ToolbarButton>
+            <ToolbarButton title={["Italic (", <kbd>Ctrl</kbd>, "+", <kbd>I</kbd>, ")"]} checked={isItalic} onClick={() => setFormat("italic")}>
+                <i className="bi bi-type-italic"></i>
+            </ToolbarButton>
+            <ToolbarButton title={["Underline (", <kbd>Ctrl</kbd>, "+", <kbd>U</kbd>, ")"]} checked={isUnderlined} onClick={() => setFormat("underline")}>
+                <i className="bi bi-type-underline"></i>
+            </ToolbarButton>
+        </>
+    );
+}
+
 function ToolbarContainer({ children }) {
     return (
         <>
-            <div className="me-2">
+            <div className="ms-2 me-2">
                 {children}
             </div>
             <div className="vr"></div>
@@ -62,11 +107,12 @@ function ToolbarContainer({ children }) {
     );
 }
 
-function ToolbarButton({ children, title, onClick = (() => { }), disabled = false }) {
+function ToolbarButton({ children, title, onClick = (() => { }), disabled = false, checked = false }) {
     return (
         <OverlayTrigger overlay={<Tooltip>{title}</Tooltip>}>
             <span className={"d-inline-block" + disabled && "cursor-not-allowed"}>
-                <Button variant="outline-secondary m-1" onClick={onClick} disabled={disabled}>{children}</Button>
+                {checked ? (<ToggleButton variant="outline-secondary m-1" onClick={onClick} disabled={disabled} checked={checked} type="radio">{children}</ToggleButton>) :
+                    <Button variant="outline-secondary m-1" onClick={onClick} disabled={disabled}>{children}</Button>}
             </span>
         </OverlayTrigger>
     );
