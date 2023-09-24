7edb950b14c02a22fe060ce250415073725dd5d4
Juanjo Asensio García <asengar2009@gmail.com>
2023-09-24 11:35:27 +0200

    Add toolbarUpdate() and support text alignemnet

--- src/components/textEditorToolbar.jsx
+++ src/components/textEditorToolbar.jsx
@@ -1,38 +1,62 @@
 import { useState, useEffect } from "react";
-import { Stack, Button, OverlayTrigger, Tooltip, ToggleButton } from "react-bootstrap";
+import { Stack, Button, OverlayTrigger, Tooltip, ToggleButton, Dropdown } from "react-bootstrap";
 import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
-import { UNDO_COMMAND, CAN_UNDO_COMMAND, REDO_COMMAND, CAN_REDO_COMMAND, SELECTION_CHANGE_COMMAND, COMMAND_PRIORITY_CRITICAL, FORMAT_TEXT_COMMAND, $getSelection, $isRangeSelection } from "lexical";
+import { UNDO_COMMAND, CAN_UNDO_COMMAND, REDO_COMMAND, CAN_REDO_COMMAND, COMMAND_PRIORITY_CRITICAL, FORMAT_TEXT_COMMAND, FORMAT_ELEMENT_COMMAND, $getSelection, $isRangeSelection, $isElementNode } from "lexical";
 
 export function Toolbar({ t }) {
+    const [editor] = useLexicalComposerContext();
+
+    const [elementFormat, setElementFormat] = useState("left");
+    const [isBold, setIsBold] = useState(false);
+    const [isItalic, setIsItalic] = useState(false);
+    const [isUnderline, setIsUnderline] = useState(false);
+    const [canUndo, setCanUndo] = useState(false);
+    const [canRedo, setCanRedo] = useState(false);
+
+    const updateToolbar = () => {
+        const selection = $getSelection();
+
+        if ($isRangeSelection(selection)) {
+            const node = selection.anchor.getNode();
+            const parent = node.getParent();
+
+            const format = $isElementNode(node) ? node.getFormatType() : parent.getFormatType();
+
+            setElementFormat(format ? format : "left");
+
+            setIsBold(selection.hasFormat("bold"));
+            setIsItalic(selection.hasFormat("italic"));
+            setIsUnderline(selection.hasFormat("underline"));
+        }
+    };
+
+    useEffect(() => {
+        editor.registerUpdateListener(({ editorState }) => {
+            editorState.read(() => {
+                updateToolbar();
+            });
+
+            editor.registerCommand(CAN_UNDO_COMMAND, (payload) => {
+                setCanUndo(payload);
+            }, COMMAND_PRIORITY_CRITICAL);
+
+            editor.registerCommand(CAN_REDO_COMMAND, (payload) => {
+                setCanRedo(payload);
+            }, COMMAND_PRIORITY_CRITICAL);
+        });
+
+    }, [editor, updateToolbar, setIsBold, setIsItalic, setIsUnderline]);
+
     return (
         <Stack direction="horizontal" className="border rounded-2 w-100 border-primary mb-2">
-            <ToolbarContainer>
-                <History />
-            </ToolbarContainer>
-            <ToolbarContainer>
-                <Format />
-            </ToolbarContainer>
+            <History t={t} editor={editor} canUndo={canUndo} canRedo={canRedo} />
+            <Format t={t} editor={editor} isBold={isBold} isItalic={isItalic} isUnderline={isUnderline} />
+            <Alignement t={t} editor={editor} elementFormat={elementFormat} />
         </Stack>
     );
 }
 
-function History() {
-    const [editor] = useLexicalComposerContext();
-
-    const [canUndo, setCanUndo] = useState(false);
-    const [canRedo, setCanRedo] = useState(false);
-
-    useEffect(() => {
-        editor.registerCommand(CAN_UNDO_COMMAND, (payload) => {
-            setCanUndo(payload);
-            return true;
-        }, COMMAND_PRIORITY_CRITICAL);
-        editor.registerCommand(CAN_REDO_COMMAND, (payload) => {
-            setCanRedo(payload);
-            return true;
-        }, COMMAND_PRIORITY_CRITICAL);
-    }, []);
-
+function History({ t, editor, canUndo, canRedo }) {
     const undo = () => {
         editor.dispatchCommand(UNDO_COMMAND);
     };
@@ -42,55 +66,74 @@ function History() {
     }
 
     return (
-        <>
+        <ToolbarContainer>
             <ToolbarButton title={["Undo (", <kbd>Ctrl</kbd>, "+", <kbd>Z</kbd>, ")"]} disabled={!canUndo} onClick={undo}>
                 <i className="bi bi-arrow-counterclockwise"></i>
             </ToolbarButton>
             <ToolbarButton title={["Redo (", <kbd>Ctrl</kbd>, "+", <kbd>Y</kbd>, ")"]} disabled={!canRedo} onClick={redo}>
                 <i className="bi bi-arrow-clockwise"></i>
             </ToolbarButton >
-        </>
+        </ToolbarContainer>
     );
 }
 
-function Format() {
-    const [editor] = useLexicalComposerContext();
-
-    const [isBold, setIsBold] = useState(false);
-    const [isItalic, setIsItalic] = useState(false);
-    const [isUnderlined, setIsUnderlined] = useState(false);
-
-    useEffect(() => {
-        editor.registerCommand(SELECTION_CHANGE_COMMAND, () => {
-            editor.update(() => {
-                const selection = $getSelection();
-
-                if ($isRangeSelection(selection)) {
-                    setIsBold(selection.hasFormat("bold"));
-                    setIsItalic(selection.hasFormat("italic"));
-                    setIsUnderlined(selection.hasFormat("underline"));
-                }
-            })
-            return true;
-        }, COMMAND_PRIORITY_CRITICAL)
-    }, [editor]);
-
+function Format({ t, editor, isBold, isItalic, isUnderline }) {
     const setFormat = (format) => {
         editor.dispatchCommand(FORMAT_TEXT_COMMAND, format);
     };
 
     return (
-        <>
+        <ToolbarContainer>
             <ToolbarButton title={["Bold (", <kbd>Ctrl</kbd>, "+", <kbd>B</kbd>, ")"]} checked={isBold} onClick={() => setFormat("bold")}>
                 <i className="bi bi-type-bold"></i>
             </ToolbarButton>
             <ToolbarButton title={["Italic (", <kbd>Ctrl</kbd>, "+", <kbd>I</kbd>, ")"]} checked={isItalic} onClick={() => setFormat("italic")}>
                 <i className="bi bi-type-italic"></i>
             </ToolbarButton>
-            <ToolbarButton title={["Underline (", <kbd>Ctrl</kbd>, "+", <kbd>U</kbd>, ")"]} checked={isUnderlined} onClick={() => setFormat("underline")}>
+            <ToolbarButton title={["Underline (", <kbd>Ctrl</kbd>, "+", <kbd>U</kbd>, ")"]} checked={isUnderline} onClick={() => setFormat("underline")}>
                 <i className="bi bi-type-underline"></i>
             </ToolbarButton>
-        </>
+        </ToolbarContainer>
+    );
+}
+
+function Alignement({ t, editor, elementFormat }) {
+    const setFormat = (format) => {
+        editor.dispatchCommand(FORMAT_ELEMENT_COMMAND, format);
+    }
+
+    const iconName = (format) => {
+        return (format == "justify" ? "justify" : ("text-" + format));
+    }
+
+    return (
+        <ToolbarContainer>
+            <Dropdown>
+                <Dropdown.Toggle variant="outline-secondary">
+                    <i className={"bi bi-" + iconName(elementFormat)}></i>
+                    {t("editor.align." + elementFormat)}
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                    <Dropdown.Item onClick={() => setFormat("left")}>
+                        <i className="bi bi-text-left"> </i>
+                        Left Align
+                    </Dropdown.Item>
+                    <Dropdown.Item onClick={() => setFormat("center")}>
+                        <i className="bi bi-text-center"> </i>
+                        Center Align
+                    </Dropdown.Item>
+                    <Dropdown.Item onClick={() => setFormat("right")}>
+                        <i className="bi bi-text-right"> </i>
+                        Right Align
+                    </Dropdown.Item>
+                    <Dropdown.Item onClick={() => setFormat("justify")}>
+                        <i className="bi bi-justify"> </i>
+                        Justify Align
+                    </Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown>
+        </ToolbarContainer>
     );
 }
 
@@ -102,7 +145,6 @@ function ToolbarContainer({ children }) {
             </div>
             <div className="vr"></div>
         </>
-
     );
 }
 
